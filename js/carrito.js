93605530f66fd7649ce78787df84ab3d119e4cca
Nico <nicolasmilitello99@gmail.com>
2026-07-03 19:03:38 +0000

    Se agrega javascript. Funciona todo pero hay que arreglar el color en los botones

---
+++ js/carrito.js
@@ -0,0 +1,74 @@
+import { obtenerCarrito } from "./storage.js";
+import { eliminarProducto, vaciarCarrito } from "./funcionesCarrito.js";
+import { actualizarContador } from "./ui.js";
+
+const renderizarCarrito = () => {
+  const carrito = obtenerCarrito();
+  actualizarContador(carrito);
+
+  const contenedor = document.getElementById("contenedor-carrito");
+  const divAcciones = document.getElementById("acciones-carrito");
+  // Resetea el carrito cada vez que se borra unb producto
+  contenedor.innerHTML = "";
+  divAcciones.innerHTML = "";
+
+  if (!carrito.length) {
+    const mensaje = document.createElement("p");
+    mensaje.classList.add("mensaje-carrito-vacio");
+    mensaje.textContent = "Tuy carrito esta vacio";
+
+    contenedor.appendChild(mensaje);
+    return;
+  }
+
+  carrito.forEach((producto, index) => {
+    const tarjeta = document.createElement("article");
+    tarjeta.classList.add("card", "text-light");
+
+    const img = document.createElement("img");
+    img.src = `../${producto.img}`;
+    img.alt = producto.alt;
+
+    const titulo = document.createElement("h3");
+    titulo.textContent = producto.nombre;
+
+    const desc = document.createElement("p");
+    desc.textContent = producto.desc;
+
+    const precio = document.createElement("p");
+    precio.textContent = `ᖬ${producto.precio}`;
+
+    const btnEliminar = document.createElement("button");
+    btnEliminar.classList.add("btn");
+    btnEliminar.classList.add("btn-eliminar-carrito");
+    btnEliminar.textContent = "Eliminar producto";
+
+    btnEliminar.addEventListener("click", () => {
+      eliminarProducto(index);
+      renderizarCarrito();
+    });
+
+    tarjeta.appendChild(img);
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(desc);
+    tarjeta.appendChild(precio);
+    tarjeta.appendChild(btnEliminar);
+
+    contenedor.appendChild(tarjeta);
+  });
+
+  const btnVaciar = document.createElement("button");
+  btnVaciar.classList.add("btn");
+  boton.classList.add("btn-vaciar-carrito");
+  boton.textContent = "Vaciar carrito";
+
+  btnVaciar.addEventListener("click", () => {
+    vaciarCarrito();
+    renderizarCarrito();
+  });
+
+  divAcciones.appendChild(btnVaciar);
+};
+document.addEventListener("DOMContentLoaded", () => {
+  renderizarCarrito();
+});
